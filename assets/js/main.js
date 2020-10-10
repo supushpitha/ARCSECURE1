5544a4230a071ab830fa7b0202ee52288e417d94
Supushpitha Atapattu <supushpitha@live.com>
2020-10-10 23:54:02 +0530

    s1

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,6 +83,123 @@
     $(".mobile-nav, .mobile-nav-toggle").hide();
   }
 
+
+
+const progress = document.querySelector('.progress-done');
+setTimeout(() => {
+  progress.style.opacity = 1;
+  progress.style.width = progress.getAttribute('data-done') + '%';
+}, 500)
+
+
+const progress1 = document.querySelector('.progress-done-1');
+setTimeout(() => {
+  progress1.style.opacity = 1;
+  progress1.style.width = progress1.getAttribute('data-done-1') + '%';
+}, 500)
+
+const progress2 = document.querySelector('.progress-done-2');
+setTimeout(() => {
+  progress2.style.opacity = 1;
+  progress2.style.width = progress2.getAttribute('data-done-2') + '%';
+}, 500)
+
+const progress3 = document.querySelector('.progress-done-3');
+setTimeout(() => {
+  progress3.style.opacity = 1;
+  progress3.style.width = progress3.getAttribute('data-done-3') + '%';
+}, 500)
+
+const progress4 = document.querySelector('.progress-done-4');
+setTimeout(() => {
+  progress4.style.opacity = 1;
+  progress4.style.width = progress4.getAttribute('data-done-4') + '%';
+}, 500)
+
+const progress5 = document.querySelector('.progress-done-5');
+setTimeout(() => {
+  progress5.style.opacity = 1;
+  progress5.style.width = progress5.getAttribute('data-done-5') + '%';
+}, 500)
+
+const progress6 = document.querySelector('.progress-done-6');
+setTimeout(() => {
+  progress6.style.opacity = 1;
+  progress6.style.width = progress6.getAttribute('data-done-6') + '%';
+}, 500)
+
+const progress7 = document.querySelector('.progress-done-7');
+setTimeout(() => {
+  progress7.style.opacity = 1;
+  progress7.style.width = progress7.getAttribute('data-done-7') + '%';
+}, 500)
+
+const progress8 = document.querySelector('.progress-done-8');
+setTimeout(() => {
+  progress8.style.opacity = 1;
+  progress8.style.width = progress8.getAttribute('data-done-8') + '%';
+}, 500)
+
+const progress9 = document.querySelector('.progress-done-9');
+setTimeout(() => {
+  progress9.style.opacity = 1;
+  progress9.style.width = progress9.getAttribute('data-done-9') + '%';
+}, 500)
+
+
+const progress10 = document.querySelector('.progress-done-10');
+setTimeout(() => {
+  progress10.style.opacity = 1;
+  progress10.style.width = progress10.getAttribute('data-done-10') + '%';
+}, 500)
+
+const progress11 = document.querySelector('.progress-done-11');
+setTimeout(() => {
+  progress11.style.opacity = 1;
+  progress11.style.width = progress11.getAttribute('data-done-11') + '%';
+}, 500)
+
+const progress12 = document.querySelector('.progress-done-12');
+setTimeout(() => {
+  progress12.style.opacity = 1;
+  progress12.style.width = progress12.getAttribute('data-done-12') + '%';
+}, 500)
+
+const progress13 = document.querySelector('.progress-done-13');
+setTimeout(() => {
+  progress13.style.opacity = 1;
+  progress13.style.width = progress13.getAttribute('data-done-13') + '%';
+}, 500)
+
+const progress14 = document.querySelector('.progress-done-14');
+setTimeout(() => {
+  progress14.style.opacity = 1;
+  progress14.style.width = progress14.getAttribute('data-done-14') + '%';
+}, 500)
+
+const progress15 = document.querySelector('.progress-done-15');
+setTimeout(() => {
+  progress15.style.opacity = 1;
+  progress15.style.width = progress15.getAttribute('data-done-15') + '%';
+}, 500)
+
+const progress16 = document.querySelector('.progress-done-16');
+setTimeout(() => {
+  progress16.style.opacity = 1;
+  progress16.style.width = progress16.getAttribute('data-done-16') + '%';
+}, 500)
+
+const progress17 = document.querySelector('.progress-done-17');
+setTimeout(() => {
+  progress17.style.opacity = 1;
+  progress17.style.width = progress17.getAttribute('data-done-17') + '%';
+}, 500)
+
+
+
+
+  
+
   // Navigation active state on scroll
   var nav_sections = $('section');
   var main_nav = $('.nav-menu, #mobile-nav');
@@ -180,6 +297,62 @@
       $('.venobox').venobox();
     });
   });
+
+
+//milestones
+$(function(){
+
+  window.sr = ScrollReveal();
+
+  if ($(window).width() < 768) {
+
+    if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+      $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+    }
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+  } else {
+    
+    sr.reveal('.js--fadeInLeft', {
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+  }
+  
+  sr.reveal('.js--fadeInLeft', {
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+
+});
+
+
 
   // Portfolio details carousel
   $(".portfolio-details-carousel").owlCarousel({
